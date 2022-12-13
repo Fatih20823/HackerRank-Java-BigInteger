3017796b0a77304784c8f4593439bbf44dbb7926
Fatih20823 <101557027+Fatih20823@users.noreply.github.com>
2022-12-14 00:28:27 +0300

    first commit

---
+++ src/com/main/Application.java
@@ -0,0 +1,16 @@
+package com.main;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Application {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        BigInteger a = new BigInteger(scan.nextLine());
+        BigInteger b = new BigInteger(scan.nextLine());
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+	}
+
+}
